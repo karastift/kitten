ec46ba066b4be8688d590b10ba3a838c2c4bcc1c
karastift <69339287+karastift@users.noreply.github.com>
2021-09-08 13:47:33 +0200

    subparsers added

---
+++ paws/arg_paw.py
@@ -0,0 +1,70 @@
+import argparse
+
+class ArgPaw:
+
+    options = None
+
+    kitten_parser = None
+    scan_parser = None
+
+    def __init__(self) -> None:
+        self.kitten_parser = self.__create_parsers()
+        self.options = self.kitten_parser.parse_args().__dict__
+
+    def __create_parsers(self) -> argparse.ArgumentParser:
+
+        kitten_parser = argparse.ArgumentParser(
+            epilog='\033[1mMade by kara.\033[0m',
+            add_help=''
+        )
+
+        self.__add_costum_help_arg(kitten_parser)
+
+        kitten_parser.add_argument(
+            '-v', '--verbose',
+            action='store_true',
+            help='Run verbosely.',
+        )
+
+        subparsers = kitten_parser.add_subparsers(
+            metavar='command',
+            dest='cmd',
+        )
+
+        self.__configure_scan_parser(subparsers)
+
+
+        return kitten_parser
+    
+    def __configure_scan_parser(self, subparsers: argparse._SubParsersAction):
+        scan_parser = subparsers.add_parser(
+            name='scan',
+            help='Scan a target for open ports.'
+        )
+        scan_parser.add_argument(
+            dest='target',
+            type=str,
+            help='Define the target that is to be scanned.',
+        )
+        scan_parser.add_argument(
+            '-mt', '--maxthreads',
+            type=int,
+            default=100,
+            help='Max number of threads that will be opened at the same time.',
+        )
+        scan_parser.add_argument(
+            '-mp', '--maxprocesses',
+            type=int,
+            default=75,
+            help='Max number of processes that will be opened at the same time.',
+        )
+    
+    def __add_costum_help_arg(self, parser: argparse.ArgumentParser):
+        parser.add_argument(
+            '-h', '--help',
+            action=argparse._HelpAction,
+            help='Show this help message and exit.',
+        )
+
+    def get_options(self):
+        return self.options
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ from time import sleep
 import timeit
 from multiprocessing.dummy import Pool
 
-target = socket.gethostbyname(sys.argv[1])
+# target = socket.gethostbyname(sys.argv[1])
 
 def is_port_open(port: int) -> bool:
     print(port)
@@ -59,11 +59,37 @@ def get_open_ports_threading() -> list:
 
 
 # implement this
-def get_open_ports_multiprocessing() -> list:
-    with Pool(75) as p:
-        return p.map(is_port_open, get_most_common_ports())
+# def get_open_ports_multiprocessing() -> list:
+#     with Pool(75) as p:
+#         return p.map(is_port_open, get_most_common_ports())
 
 
-t1 = timeit.default_timer()
-print(list(filter(None, get_open_ports_multiprocessing())))
-print(timeit.default_timer() - t1)
+# t1 = timeit.default_timer()
+# print(list(filter(None, get_open_ports_multiprocessing())))
+# print(timeit.default_timer() - t1)
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Glücksspiel")
+subparsers = parser.add_subparsers()
+# wuerfeln-Befehle
+parser_wuerfeln = subparsers.add_parser("wuerfeln", help="Simulation eines Würfels")
+parser_wuerfeln.add_argument("--wuerfe", "-w", type=int, default=1, help="Anzahl der Würfe")
+parser_wuerfeln.add_argument("--seiten", "-s", type=int, choices=[6, 12, 20], default=6, help="Seitenanzahl des Würfels")
+parser_wuerfeln.add_argument("--schummeln", "-sch", action="store_true", help="Schummel-Flag")
+parser_wuerfeln.set_defaults(func=wuerfeln_fct)
+# muenze-Befehl
+parser_muenze = subparsers.add_parser("muenze", help="Simulation von Münzwürfen")
+parser_muenze.add_argument("--wuerfe", "-w", type=int, default=1, help="Anzahl der Würfe")
+parser_muenze.set_defaults(func=muenze_fct)
+# lotto-Befehl
+parser_lotto = subparsers.add_parser("lotto", help="Simulation von Lotto 6 aus 49")
+parser_lotto.add_argument("--zahlen", "-z", type=int, nargs=6, choices=np.arange(1, 50), required=True, help="Deine 6 Zahlen")
+parser_lotto.add_argument(    "--superzahl", "-sz", type=int, choices=np.arange(0, 10), required=True, help="Superzahl")
+parser_lotto.set_defaults(func=lotto_fct)
+args = parser.parse_args()
+try:
+    args.func(args)
+except AttributeError:
+    parser.print_help()
+    parser.exit()
