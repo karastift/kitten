811cf8fac2c1963c970360d8208e92fe488b307d
Kian van der Meer <kiansinan@gmail.com>
2021-09-08 17:23:17 +0200

    added main function

--- setup.py
+++ setup.py
@@ -1,7 +1,25 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='kitten',
-    version='beta',
+    version='0.1',
+    url='https://github.com/karastift/kitten.git',
+    author='kara',
     py_modules=['argparse', 'socket', 'json', 'time', 'multiprocessing', 'threading'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3 :: Only',
+    ],
+    package_dir={'': 'kitten'},
+    packages=find_packages(where='kitten'),
+    python_requires='>=3.6, <4',
+    package_data={
+        'sample': ['port_data.json'],
+    },
 )
