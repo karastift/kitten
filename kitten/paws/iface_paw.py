bed3780c2c2023aff95952278f76ae74345a28f6
kara <kiansinan@gmail.com>
2021-10-13 11:14:43 +0200

    use getter und setter in main instead __init__

--- kitten/paws/iface_paw.py
+++ kitten/paws/iface_paw.py
@@ -1,47 +1,40 @@
 import subprocess
-from types import prepare_class
 
 from paws.util_paw import UtilPaw
 
 class IfacePaw:
 
-    options = None
-
-    _util_paw = None
+    __util_paw = None
     
-    _interface = None
-    _automode = None
-    _prev_mode = 'monitor'
+    _interface = ''
+    _automode = bool()
 
     def __init__(self, options, util_paw: UtilPaw) -> None:
-        self.options = options
-        self._util_paw = util_paw
+        self.__util_paw = util_paw
 
-    def set_automode(self, automode: bool):
+    def set_automode(self, automode: bool) -> None:
         self._automode = automode
     
-    def set_interface(self, interface: str):
+    def set_interface(self, interface: str) -> None:
 
-        interface = self.get_interface(interface)
+        interface = self.get_interface_by_name(interface)
         self._interface = interface['name']
 
         if self._automode and interface['mode'] != 'monitor':
-            self._prev_mode = interface['mode']
             self.switch_interface_mode('monitor')
         
         if not self._automode and interface['mode'] == 'managed':
-            self._util_paw.print_text('Your interface is in managed mode. Some scans require monitor mode. So if you get no results that could be the reason. Set the -am flag to automatically change the mode into the required one.', color='yellow', attrs=['bold'])
+            self.__util_paw.print_text('Your interface is in managed mode. Some scans require monitor mode. So if you get no results that could be the reason. Set the -am flag to automatically change the mode into the required one.', color='yellow', attrs=['bold'])
         
 
-    def get_interface(self, name: str):
+    def get_interface_by_name(self, name: str) -> str:
         interfaces = self.get_interfaces()
         return next(iface for iface in interfaces if iface['name'] == name)
     
-    def get_selected_interface(self):
+    def get_selected_interface(self) -> str:
         return self._interface
 
-        
-    def get_interfaces(self):
+    def get_interfaces(self) -> list:
         output = subprocess.getoutput('iwconfig').split('\n\n')
 
         interfaces = []
@@ -69,8 +62,7 @@ class IfacePaw:
         
         return interfaces
 
-
-    def switch_interface_mode(self, mode: str):
+    def switch_interface_mode(self, mode: str) -> None:
         assert mode in {'monitor', 'managed'}, f'Invalid mode "{mode}"'
 
         try:
@@ -85,8 +77,8 @@ class IfacePaw:
             subprocess.Popen(f'iwconfig {self._interface} mode {mode}'.split(' ')).wait()
             subprocess.Popen(f'ifconfig {self._interface} up'.split(' ')).wait()
 
-            self._util_paw.print_text(f'\nPut {self._interface} into {mode} mode.', end='\n\n', color='white', attrs=['bold'])
+            self.__util_paw.print_text(f'\nPut {self._interface} into {mode} mode.', end='\n\n', color='white', attrs=['bold'])
 
         except PermissionError:
-            self._util_paw.print_permission_error()
+            self.__util_paw.print_permission_error()
             exit()
--- kitten/paws/scan_paw.py
+++ kitten/paws/scan_paw.py
@@ -1,8 +1,9 @@
 from multiprocessing.dummy import Pool
-import subprocess
 import threading
 import socket
 from time import sleep
+from scapy.layers.dot11 import Dot11, Dot11Beacon, Dot11Elt, sniff, Packet
+
 from paws.iface_paw import IfacePaw
 from paws.util_paw import UtilPaw
 
@@ -10,55 +11,36 @@ class ScanPaw(IfacePaw):
 
     _util_paw = None
 
-    # scapy imports
-    Dot11Beacon = None
-    Dot11 = None
-    Dot11Elt = None
-    sniff = None
-
     # port scanning
     _target = None
     _maxprocesses = None
     _maxthreads = None
 
     # scanning for networks
-    _networks_found = {}
-    _prev_length = 0
+    __networks_found = {}
 
     def __init__(self, options, util_paw: UtilPaw) -> None:
         self._util_paw = util_paw
+        super().__init__(options, self._util_paw)
 
-        method = options['mthd']
-        if method == 'ports':
-            self.__set_target(options['target'])
-            self.__set_maxprocesses(options['maxprocesses'])
-            self.__set_maxthreads(options['maxthreads'])
-        
-        elif method == 'networks':
-            self.set_automode(options['automode'])
-            self.set_interface(options['interface'])
-            super().__init__(options, self._util_paw)
-            self.__init_scapy_util()
-
-    def __init_scapy_util(self):
-        scapy_all = __import__('scapy.all', fromlist=['Dot11Beacon', 'Dot11', 'Dot11Elt', 'sniff'])
-        self.Dot11 = scapy_all.Dot11
-        self.Dot11Beacon = scapy_all.Dot11Beacon
-        self.Dot11Elt = scapy_all.Dot11Elt
-        self.sniff = scapy_all.sniff
-
-    def __set_target(self, target: str) -> None:
+    def set_target(self, target: str) -> None:
         try:
             self._target = socket.gethostbyname(target)
         except socket.gaierror:
             self._util_paw.print_text('Hostname could not be resolved.', color='red')
             exit()
 
-    def __set_maxthreads(self, maxthreads: int) -> None:
+    def set_maxthreads(self, maxthreads: int) -> None:
         self._maxthreads = maxthreads
 
-    def __set_maxprocesses(self, maxprocesses: int) -> None:
+    def set_maxprocesses(self, maxprocesses: int) -> None:
         self._maxprocesses = maxprocesses
+    
+    def set_interface(self, interface: str) -> None:
+        return super().set_interface(interface)
+
+    def set_automode(self, automode: bool) -> None:
+        return super().set_automode(automode)
 
     def is_open_port(self, port: int) -> bool:
 
@@ -122,32 +104,29 @@ class ScanPaw(IfacePaw):
 
         return open_ports
 
-
-
-
-    def __handle_packet(self, packet):
-        if packet.haslayer(self.Dot11Beacon):
+    def __handle_packet(self, packet: Packet) -> None:
+        if packet.haslayer(Dot11Beacon):
             
-            bssid = packet[self.Dot11].addr2
+            bssid = packet[Dot11].addr2
             
             try:
-                ssid = packet[self.Dot11Elt].info.decode()
+                ssid = packet[Dot11Elt].info.decode()
             except UnicodeDecodeError:
-                ssid = packet[self.Dot11Elt].info
+                ssid = packet[Dot11Elt].info
             
             try:
                 dbm_signal = packet.dBm_AntSignal
             except:
                 dbm_signal = 'N/A'
             
-            stats = packet[self.Dot11Beacon].network_stats()
+            stats = packet[Dot11Beacon].network_stats()
             
             channel = stats.get('channel')
             
             crypto = stats.get('crypto')
 
-            if bssid not in self._networks_found.keys():
-                self._networks_found[bssid] = {
+            if bssid not in self.__networks_found.keys():
+                self.__networks_found[bssid] = {
                     'bssid': bssid,
                     'ssid': ssid,
                     'dbm_signal': dbm_signal,
@@ -155,18 +134,18 @@ class ScanPaw(IfacePaw):
                     'crypto': list(crypto)[0]
                 }
                 
-                self._util_paw.print_scanned_network(self._networks_found[bssid])
+                self._util_paw.print_scanned_network(self.__networks_found[bssid])
 
-    def get_wireless_networks(self):
+    def scan_for_wireless_networks(self) -> None:
         try:
-            self.sniff(prn=self.__handle_packet, iface=self.get_selected_interface())
+            sniff(prn=self.__handle_packet, iface=self.get_selected_interface())
 
         except KeyboardInterrupt:
 
-            self.switch_interface_mode(self._prev_mode)
-            return self._networks_found
+            self.switch_interface_mode(self.__prev_mode)
+            return self.__networks_found
         
         except PermissionError:
-            self.switch_interface_mode(self._prev_mode)
+            self.switch_interface_mode(self.__prev_mode)
             self._util_paw.print_permission_error()
             exit()
